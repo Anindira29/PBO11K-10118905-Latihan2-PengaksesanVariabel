ce17cae9c2795165c69737aa26ee695b9467c560
Fitri <Fitri@DESKTOP-5GMG7NE>
2019-09-24 08:46:35 -0700

    Latian1-Biodata

---
+++ src/latihan2/Latihan2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author 
+ * NAMA         : Anindira Dina Fitriani
+ * Kelas        : IF11 - K
+ * DEKLARASI    : Pengaksesan Variabel
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI =3.14; 
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //MENAMPILKAN HASIL
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = "+ nilaiInt);
+        System.out.println("Isi Variabel PHI = "+ PHI);
+        System.out.println("Isi Variabel Logika = "+ nilaiLogika);
+        System.out.println("Isi Variabel Karakter = "+ nilaiKarakter);
+        
+        
+    }
+    
+}
